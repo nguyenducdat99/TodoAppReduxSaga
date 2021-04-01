55d3ecc776ddd5e0ff9b2de2912fb7f83596f742
nguyenducdat99 <nguyenducdat10101999@gmail.com>
2021-04-01 17:54:55 +0700

    complete toto-form 5:54pm 01/04/2021

---
+++ src/components/todoForm/TodoForm.js
@@ -0,0 +1,38 @@
+// import style library
+import './TodoForm.scss';
+
+function TodoForm() {
+    return (
+        <div className="todo-form">
+            <form action="" method="">
+                <div className='todo-form__form-group'>
+                    <h3>Thêm sản phẩm</h3>
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                </div>
+                <div className='todo-form__form-group'>
+                        <input type='text'
+                            className='todo-form__form-control'
+                            placeholder="Tên sản phẩm"
+                        />
+                </div>
+                <div className='todo-form__form-group'>
+                        <input type='number'
+                            className='todo-form__form-control'
+                            placeholder="Giá"
+                            min={0}
+                        />
+                </div>
+                <div className='todo-form__form-group'>
+                        <label>
+                            <input type="radio" /> Kích hoạt
+                        </label>&nbsp;&nbsp;
+                        <label>
+                            <input type="radio"/> Ẩn
+                        </label>
+                </div>
+                <button type='submit' className="todo-form__submit">Thêm</button>
+            </form>
+        </div>
+    )
+}
+export default TodoForm;
